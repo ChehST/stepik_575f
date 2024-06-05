66cd0abfe807cd7166851ec3b4ed906eda32a6f8
ChehST <chehstream@gmail.com>
2024-06-05 18:28:15 +0400

    add login page checks

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +13,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_url_in_string(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_page()
