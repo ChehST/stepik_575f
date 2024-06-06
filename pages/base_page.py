58a7d33773ebe2008654ff057b88f30d884361f9
ChehST <chehstream@gmail.com>
2024-06-06 18:23:35 +0400

    replace common methods in BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException,
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from .locators import BasePageLocators
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -25,6 +27,13 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
