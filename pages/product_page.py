35a700459af2a176454d8812fb3553aae86b4d99
ChehST <chehstream@gmail.com>
2024-06-06 18:10:05 +0400

    add message box checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,12 +9,16 @@ class ProductPage(BasePage):
     def should_add_product_to_cart_from_its_promo_page(self):
         # check url
         self.should_be_promo_url()
+        self.should_not_be_success_message()
         # add to cart
-        self.find(*ProductPageLocators.ADD_TO_CART_BTN).click()
-        self.solve_quiz_and_get_code()
+        
         self.should_be_succes_message_on_the_product_add()
         self.should_basket_increased_on_the_products_price_values
     
+    def add_the_product_to_cart(self):
+        self.find(*ProductPageLocators.ADD_TO_CART_BTN).click()
+        self.solve_quiz_and_get_code()
+
     def should_be_promo_url(self):
         assert "?promo" in self.browser.current_url , "It isn't promo!"
 
@@ -28,5 +32,12 @@ class ProductPage(BasePage):
         msg = ("Your basket total is now" + product_price)
         assert msg in self.get_text(*ProductPageLocator.MSG_BUSKET_VOLUME), f'{msg}'
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MSG_PRD), \
+       "Success message is presented, but should not be"
+
+    def should_not_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.MSG_PRD), \
+        "Message disappearces, but shouldn't!"
 
     
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,9 @@
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 
-
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -14,8 +15,30 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser,link):
-    # link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser,link)
     page.open()
     page.should_add_product_to_cart_from_its_promo_page()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_the_product_to_cart()
+    page.should_not_be_success_message()
+ 
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser,link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_the_product_to_cart()
+    page.should_not_disappeared()
+
     
